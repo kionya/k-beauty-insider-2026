45f4f8b5ed5b65470545d6b0091e0926f30686be
kiony <konya340@gmail.com>
2026-01-28 16:39:28 +0900

    Retry: Robots txt fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
     images: ['/og-image.png'],
   },
   verification: {
-    google: 'XO6iCCPRtNaESiPjpDjLSC6nTkVojbv9DvblIu0sGJ8>',
+    google: 'XO6iCCPRtNaESiPjpDjLSC6nTkVojbv9DvblIu0sGJ8',
   },
 };
 
